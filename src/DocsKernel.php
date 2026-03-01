54374f15b4eca6e061f80df7c569c5049176414e
Javier Eguiluz <javier.eguiluz@gmail.com>
2026-03-01 12:02:28 +0100

    Restore admonition icons and delete some unused files

--- src/DocsKernel.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SymfonyDocsBuilder;
-
-// This file has been replaced by GuidesContainerFactory.php as part of the
-// migration from doctrine/rst-parser to phpdocumentor/guides.
-// It is kept empty to avoid autoload errors during the transition.
--- src/SymfonyHTMLFormat.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SymfonyDocsBuilder;
-
-// This file has been replaced by custom NodeRenderers registered in
-// GuidesContainerFactory.php as part of the migration from
-// doctrine/rst-parser to phpdocumentor/guides.
-// It is kept empty to avoid autoload errors during the transition.
